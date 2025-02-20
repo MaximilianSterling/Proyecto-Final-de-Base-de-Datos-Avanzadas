7c6e0415c64d05339e84765c29269564f53b6cc3
normabeltraan <norma07beltran@hotmail.com>
2025-02-19 18:49:25 -0700

    Hice CitaBO, CitaMapper, y edite y agregue cosas que faltaban

--- capaNegocioProyecto1/src/main/java/Configuracion/DependencyInjector.java
+++ capaNegocioProyecto1/src/main/java/Configuracion/DependencyInjector.java
@@ -21,39 +21,39 @@ import conexion.IConexionBD;
  */
 public class DependencyInjector 
 {
-    /**
-     * 
-     * @return 
-     */
-    public static PacienteBO crearPacienteBO() {
-        IConexionBD conexion = new ConexionBD();
-        PacienteBO pacienteBO = new PacienteBO(conexion);
-
-        return pacienteBO;
-    }
-    
-    /**
-     * 
-     * @return 
-     */
-    public static MedicoBO crearMedicoBO() {
-        IConexionBD conexion = new ConexionBD();
-        MedicoBO medicoBO = new MedicoBO(conexion);
-
-        return medicoBO;
-    }
-    
-    /**
-     * 
-     * @return 
-     */
-    public static UsuarioBO crearUsuarioBO() {
-        IConexionBD conexion = new ConexionBD();
-        UsuarioBO usuarioBO = new UsuarioBO(conexion);
-
-        return usuarioBO;
-    }
-    
+//    /**
+//     * 
+//     * @return 
+//     */
+//    public static PacienteBO crearPacienteBO() {
+//        IConexionBD conexion = new ConexionBD();
+//        PacienteBO pacienteBO = new PacienteBO(conexion);
+//
+//        return pacienteBO;
+//    }
+//    
+//    /**
+//     * 
+//     * @return 
+//     */
+//    public static MedicoBO crearMedicoBO() {
+//        IConexionBD conexion = new ConexionBD();
+//        MedicoBO medicoBO = new MedicoBO(conexion);
+//
+//        return medicoBO;
+//    }
+//    
+//    /**
+//     * 
+//     * @return 
+//     */
+//    public static UsuarioBO crearUsuarioBO() {
+//        IConexionBD conexion = new ConexionBD();
+//        UsuarioBO usuarioBO = new UsuarioBO(conexion);
+//
+//        return usuarioBO;
+//    }
+//    
     /**
      * 
      * @return 
@@ -65,47 +65,47 @@ public class DependencyInjector
         return citaBO;
     }
     
-    /**
-     * 
-     * @return 
-     */
-    public static CitaSinCitaBO crearCitaSinCitaBO() {
-        IConexionBD conexion = new ConexionBD();
-        CitaSinCitaBO citaSinCitaBO = new CitaSinCitaBO(conexion);
-
-        return citaSinCitaBO;
-    }
-    
-    /**
-     * 
-     * @return 
-     */
-    public static ConsultaBO crearConsultaBO() {
-        IConexionBD conexion = new ConexionBD();
-        ConsultaBO consultaBO = new ConsultaBO(conexion);
-
-        return consultaBO;
-    }
-    
-    /**
-     * 
-     * @return 
-     */
-    public static DireccionBO crearDireccionBO() {
-        IConexionBD conexion = new ConexionBD();
-        DireccionBO direccionBO = new DireccionBO(conexion);
-
-        return direccionBO;
-    }
-    
-    /**
-     * 
-     * @return 
-     */
-    public static HorarioBO crearHorarioBO() {
-        IConexionBD conexion = new ConexionBD();
-        HorarioBO horarioBO = new HorarioBO(conexion);
-
-        return horarioBO;
-    }
+//    /**
+//     * 
+//     * @return 
+//     */
+//    public static CitaSinCitaBO crearCitaSinCitaBO() {
+//        IConexionBD conexion = new ConexionBD();
+//        CitaSinCitaBO citaSinCitaBO = new CitaSinCitaBO(conexion);
+//
+//        return citaSinCitaBO;
+//    }
+//    
+//    /**
+//     * 
+//     * @return 
+//     */
+//    public static ConsultaBO crearConsultaBO() {
+//        IConexionBD conexion = new ConexionBD();
+//        ConsultaBO consultaBO = new ConsultaBO(conexion);
+//
+//        return consultaBO;
+//    }
+//    
+//    /**
+//     * 
+//     * @return 
+//     */
+//    public static DireccionBO crearDireccionBO() {
+//        IConexionBD conexion = new ConexionBD();
+//        DireccionBO direccionBO = new DireccionBO(conexion);
+//
+//        return direccionBO;
+//    }
+//    
+//    /**
+//     * 
+//     * @return 
+//     */
+//    public static HorarioBO crearHorarioBO() {
+//        IConexionBD conexion = new ConexionBD();
+//        HorarioBO horarioBO = new HorarioBO(conexion);
+//
+//        return horarioBO;
+//    }
 }
