8e5b7cf15517d31d98b92406bd79d76d88ee7bd4
Maximiliano <maximilianoreynaaguilar@gmail.com>
2025-02-19 19:11:07 -0600

    Horario chequeo

--- capaNegocioProyecto1/src/main/java/Mapper/CitaMapper.java
+++ capaNegocioProyecto1/src/main/java/Mapper/CitaMapper.java
@@ -4,11 +4,52 @@
  */
 package Mapper;
 
+import DTO.CitaDTO;
+import entidades.Cita;
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Maximiliano
  */
 public class CitaMapper 
 {
-    
+    /**
+     * Convierte un ActivistaNuevoDTO a una entidad Activista
+     *
+     * @param cita activistaNuevo que se quiere convertir
+     * @return cita entidad
+     */
+    public Cita toEntity(CitaDTO cita) {
+        if (cita == null) {
+            return null;
+        }
+        return new Cita(
+                cita.getId_cita(),
+                cita.getFecha_hora(),
+                cita.getEstado(),
+                cita.getTipo(),
+                cita.getPaciente(),
+                cita.getMedico()
+        );
+    }
+
+    /**
+     * Convierte una entidad Activista a un ActivistaNuevoDTO
+     *
+     * @param activista activista entiedad que se quiere convertir
+     * @return activistaNuevo
+     */
+    public ActivistaNuevoDTO toNuevoDTO(Activista activista) {
+        if (activista == null) {
+            return null;
+        }
+        return new ActivistaNuevoDTO(
+                activista.getNombre(),
+                activista.getApellidoPaterno(),
+                activista.getApellidoMaterno(),
+                activista.getTelefono(),
+                activista.getFechaInicio()
+        );
+    }
 }
